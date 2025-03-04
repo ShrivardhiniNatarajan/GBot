2934038bec5eb1a84b4e6ebdaf50739e2a0e5172
ShrivardhiniNatarajan <shrivardhini.n2024@vitstudent.ac.in>
2025-03-05 00:22:17 +0530

    Delete configs directory

--- configs/DefaultConfig.py
+++
@@ -1,9 +0,0 @@
-import configparser
-
-config=configparser.ConfigParser()
-config.read(r'C:\Users\shriv\OneDrive\Desktop\GoogleBot\config.ini')
-
-DISCORD_OWNER_ID=config['DEFAULT'].get('discord_owner_id')
-DISCORD_SDK=config['DEFAULT'].get('discord_sdk')
-GEMINI_SDK=config['DEFAULT'].get('gemini_sdk')
-#print(f"Discord SDK Token:", DISCORD_SDK)
